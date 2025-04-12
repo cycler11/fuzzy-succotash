52b47119bd1cafe75b7ca4ff719d112108c09008
Andrey <152691772+cycler11@users.noreply.github.com>
2025-04-12 10:02:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-import re
+def assembler_to_binary(asm_code):
+    # slovar
+    opcode_dict = {
+        'CLR A': 'E4',
+        'DEC A': '14',
+        'DEC R7': '1F',
+    }
+    
+    result = ''
+    lines = asm_code.strip().split('\n')
+    
+    for line in lines:
+        cmd = line.strip()
+        if cmd in opcode_dict:
+            result += opcode_dict[cmd]
+        else:
+            print(f'Неизвестня команда: {cmd}')
+    
+    return result
+
+asm_code = """\
+CLR A
+DEC A
+DEC R7
+"""
+
+binary_output = assembler_to_binary(asm_code)
+print(binary_output)  
+
